4599f0595c9698b9434878a81aea39c808f788d7
INSANE-P <chanbin0626@gmail.com>
2026-02-09 02:26:12 +0900

    test: 유효성 검사 및 게임 종료 테스트

---
+++ test/e2e/validation.cy.js
@@ -0,0 +1,27 @@
+describe('유효성 검사 테스트', () => {
+  beforeEach(() => {
+    cy.startGame();
+  });
+
+  it('선택 처리 중에는 선택지 버튼이 비활성화된다', () => {
+    cy.drawCard();
+    cy.chooseA();
+    cy.get('#btn-choice-a').should('be.disabled');
+    cy.get('#btn-choice-b').should('be.disabled');
+  });
+
+  it('선택 처리 완료 후 카드 뽑기 버튼이 활성화된다', () => {
+    cy.drawAndChooseA();
+    cy.get('#btn-draw').should('not.be.disabled');
+  });
+
+  it('식량이 0 미만으로 내려가지 않는다', () => {
+    cy.drawAndChooseA();
+    cy.drawAndChooseA();
+    cy.drawAndChooseA();
+    cy.getStat('food').invoke('text').then((text) => {
+      const food = parseInt(text, 10);
+      expect(food).to.be.at.least(0);
+    });
+  });
+});
