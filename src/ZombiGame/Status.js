1dd00e051ebbc72456661a32a4fe15d2052ecafb
realcdh <rdhdevelop@gmail.com>
2026-03-30 13:31:43 +0900

    feat: Status 클래스 추가 및 초기 스탯 설정

---
+++ src/ZombiGame/Status.js
@@ -0,0 +1,22 @@
+export default class Status {
+    constructor(day, hp, food, infection, heal, rescue) {
+        this.day = day;
+        this.hp = hp;
+        this.food = food;
+        this.infection = infection;
+        this.heal = heal;
+        this.rescue = rescue;
+
+        this.initStat();
+    }
+
+    // 게임 시작 시 초기 스탯 설정
+    initStat() {
+        this.day = 1;
+        this.hp = 100;
+        this.food = 3;
+        this.infection = 10;
+        this.heal = 0;
+        this.rescue = 0;
+    }
+}
