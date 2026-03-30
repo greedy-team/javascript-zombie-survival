490330c62e4e9391f66ba1549f66fb77009c3687
EM-H20 <ghd0106@gmail.com>
2026-03-30 19:22:42 +0900

    feat: GameModel 초기 상태 설정

--- src/model.js
+++ src/model.js
@@ -1,3 +1,19 @@
+import {
+  INITIAL_HP,
+  INITIAL_FOOD,
+  INITIAL_INFECTION,
+  INITIAL_DAY,
+  INITIAL_HEAL_ATTEMPTS,
+  INITIAL_RESCUE_POINTS,
+} from "./constants.js";
+
 export class GameModel {
-  constructor() {}
+  constructor() {
+    this.hp = INITIAL_HP;
+    this.food = INITIAL_FOOD;
+    this.infection = INITIAL_INFECTION;
+    this.day = INITIAL_DAY;
+    this.healAttempts = INITIAL_HEAL_ATTEMPTS;
+    this.rescuePoints = INITIAL_RESCUE_POINTS;
+  }
 }
