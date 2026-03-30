36a920ccadc2a681790f5d1f4c69152a24ca229d
EM-H20 <ghd0106@gmail.com>
2026-03-30 20:06:00 +0900

    feat: 덱 생성, 셔플, 카드 뽑기 기능 추가

--- src/model.js
+++ src/model.js
@@ -5,6 +5,7 @@ import {
   INITIAL_DAY,
   INITIAL_HEAL_ATTEMPTS,
   INITIAL_RESCUE_POINTS,
+  CARD_DATA,
 } from "./constants.js";
 
 export class GameModel {
@@ -15,5 +16,35 @@ export class GameModel {
     this.day = INITIAL_DAY;
     this.healAttempts = INITIAL_HEAL_ATTEMPTS;
     this.rescuePoints = INITIAL_RESCUE_POINTS;
+    this.deck = this.generateDeck();
+  }
+
+  generateDeck() {
+    const deck = [];
+    for (const card of CARD_DATA) {
+      for (let i = 0; i < card.count; i += 1) {
+        deck.push(card);
+      }
+    }
+    this.shuffleDeck(deck);
+    return deck;
+  }
+
+  shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i -= 1) {
+      // 0 ~ TOTAL_DECK_SIZE-1 사이의 랜덤 인덱스
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      const tmp = deck[i];
+      deck[i] = deck[randomIndex];
+      deck[randomIndex] = tmp;
+    }
+  }
+
+  drawCard() {
+    // 덱이 비면 새로 생성
+    if (this.deck.length === 0) {
+      this.deck = this.generateDeck();
+    }
+    return this.deck.pop();
   }
 }
