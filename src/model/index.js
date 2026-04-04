ba21a986d395315372209439dc37b9bab199d8c0
rahwan10 <donggeon10@naver.com>
2026-04-04 21:40:33 +0900

    fix:리뷰티키타카 수정

--- src/model/index.js
+++ src/model/index.js
@@ -1,148 +1,146 @@
-const cardData = { //모각코 후 수정
-    0: {
-        name: "생존자 시체",
-        desc: "생존자 시체",
-        choiceA: "배낭째로 가져온다: 식량 +3, 감염 +8",
-        resultA: "식량 +3, 감염도 +8",
-        choiceB: "겉에 있는 것만 집는다: 식량 +1",
-        resultB: "식량 +1",
-        A: {
-            food: +3,
-            infection: +8
-        },
-        B: {
-            food: +1
-        }
+const cardData = {
+  // 모각코 후 수정
+  0: {
+    name: '생존자 시체',
+    desc: '생존자 시체',
+    choiceA: '배낭째로 가져온다: 식량 +3, 감염 +8',
+    resultA: '식량 +3, 감염도 +8',
+    choiceB: '겉에 있는 것만 집는다: 식량 +1',
+    resultB: '식량 +1',
+    A: {
+      food: +3,
+      infection: +8,
     },
-    1: {
-        name: "부상당한 군인",
-        desc: "부상당한 군인",
-        choiceA: "식량을 건네고 약을 받는다: 식량 -1, 감염 -20",
-        resultA: "식량 -1, 감염도 -20",
-        choiceB: "몸싸움 끝에 식량만 챙기고 떠난다: 체력 -10, 식량 +2",
-        resultB: "체력 -10, 식량 +2",
-        A: {
-            food: -1,
-            infection: -20,
-            heal: +1
-        },
-        B: {
-            hp: -10,
-            food: +2
-        }
+    B: {
+      food: +1,
     },
-    2: {
-        name: "임시 수술",
-        desc: "임시 수술",
-        choiceA: "감염 부위를 직접 도려낸다: 체력 -25, 감염 -25",
-        resultA: "체력 -25, 감염도 -25",
-        choiceB: "이를 악물고 참는다: 체력 -5, 감염 +10",
-        resultB: "체력 -5, 감염도 +10",
-        A: {
-            hp: -25,
-            infection: -25,
-            heal: +1
-        },
-        B: {
-            hp: -5,
-            infection: +10
-        }
+  },
+  1: {
+    name: '부상당한 군인',
+    desc: '부상당한 군인',
+    choiceA: '식량을 건네고 약을 받는다: 식량 -1, 감염 -20',
+    resultA: '식량 -1, 감염도 -20',
+    choiceB: '몸싸움 끝에 식량만 챙기고 떠난다: 체력 -10, 식량 +2',
+    resultB: '체력 -10, 식량 +2',
+    A: {
+      food: -1,
+      infection: -20,
+      heal: +1,
     },
-    3: {
-        name: "군용 차량 행렬",
-        desc: "군용 차량 행렬",
-        choiceA: "뛰어나가 신호를 보낸다: 구조 +1, 감염 +8",
-        resultA: "구조 +1, 감염도 +8",
-        choiceB: "몸을 낮추고 방향만 확인한다: 체력 +5",
-        resultB: "체력 +5",
-        A: {
-            rescuePoint: +1,
-            infection: +8
-        },
-        B: {
-            hp: +5,
-        }
+    B: {
+      hp: -10,
+      food: +2,
     },
-    4: {
-        name: "오염된 웅덩이",
-        desc: "오염된 웅덩이",
-        choiceA: "그냥 마신다: 체력 +5, 감염 +15",
-        resultA: "체력 +5, 감염도 +15",
-        choiceB: "참는다. 빗물을 기다린다: 체력 -10",
-        resultB: "체력 -10",
-        A: {
-            hp: +5,
-            infection: +15
-        },
-        B: {
-            hp: -10,
-        }
+  },
+  2: {
+    name: '임시 수술',
+    desc: '임시 수술',
+    choiceA: '감염 부위를 직접 도려낸다: 체력 -25, 감염 -25',
+    resultA: '체력 -25, 감염도 -25',
+    choiceB: '이를 악물고 참는다: 체력 -5, 감염 +10',
+    resultB: '체력 -5, 감염도 +10',
+    A: {
+      hp: -25,
+      infection: -25,
+      heal: +1,
     },
-    5: {
-        name: "구조 트럭",
-        desc: "구조 트럭",
-        choiceA: "전력으로 달려간다: 체력 -20, 구조 +1",
-        resultA: "체력 -20, 구조 포인트 +1",
-        choiceB: "체력을 아끼고 쉰다: 체력 +10",
-        resultB: "체력 +10",
-        A: {
-            hp: -20,
-            rescuePoint: +1
-        },
-        B: {
-            hp: +10,
-        }
-    }
+    B: {
+      hp: -5,
+      infection: +10,
+    },
+  },
+  3: {
+    name: '군용 차량 행렬',
+    desc: '군용 차량 행렬',
+    choiceA: '뛰어나가 신호를 보낸다: 구조 +1, 감염 +8',
+    resultA: '구조 +1, 감염도 +8',
+    choiceB: '몸을 낮추고 방향만 확인한다: 체력 +5',
+    resultB: '체력 +5',
+    A: {
+      rescuePoint: +1,
+      infection: +8,
+    },
+    B: {
+      hp: +5,
+    },
+  },
+  4: {
+    name: '오염된 웅덩이',
+    desc: '오염된 웅덩이',
+    choiceA: '그냥 마신다: 체력 +5, 감염 +15',
+    resultA: '체력 +5, 감염도 +15',
+    choiceB: '참는다. 빗물을 기다린다: 체력 -10',
+    resultB: '체력 -10',
+    A: {
+      hp: +5,
+      infection: +15,
+    },
+    B: {
+      hp: -10,
+    },
+  },
+  5: {
+    name: '구조 트럭',
+    desc: '구조 트럭',
+    choiceA: '전력으로 달려간다: 체력 -20, 구조 +1',
+    resultA: '체력 -20, 구조 포인트 +1',
+    choiceB: '체력을 아끼고 쉰다: 체력 +10',
+    resultB: '체력 +10',
+    A: {
+      hp: -20,
+      rescuePoint: +1,
+    },
+    B: {
+      hp: +10,
+    },
+  },
 };
-
-function whatIsCardContent(card) {
-    return cardData[card]
+const status = {
+  DEADBODY: 0,
+  SOLDIER: 1,
+  OPERATION: 2,
+  CAR: 3,
+  POOL: 4,
+  RESCUETRUCK: 5,
+};
+function loadCardContent(card) {
+  return cardData[card];
 }
-function shuffleCard() {
-    let tmpCards = [];
-    for (let i = 0; i < 20; i++) {
-        tmpCards.push(i);
-    }
-    console.log(tmpCards);
-    return tmpCards
+function createCardPack() {
+  const tmpCards = [];
+  for (let i = 0; i < 20; i += 1) {
+    tmpCards.push(i);
+  }
+  return tmpCards;
 }
-function pickShuffleCard(cards) { //model
-    let index;
-    index = Math.floor(Math.random() * cards.length);
-    let card = cards.splice(index, 1)[0];
-    console.log(card);
-    if (card < 4) { //생존자 시체
-        return 0;
-    } else if (card < 8) { //부상당한 군인
-        return 1;
-    } else if (card < 11) {//임시수술
-        return 2;
-    } else if (card < 14) {//군용 차량 행렬
-        return 3;
-    } else if (card < 17) {// 오염된 웅덩이
-        return 4;
-    } else if (card < 20) {//구조 트럭
-        return 5;
-    }
-    return 0;
-}
-function checkEnding(player) {
-    let ending;
-    if (player.hp === 0) {
-        ending = "사망";
-    } else if (player.infection >= 100) {
-        ending = "좀비화";
-    } else if (player.heal >= 5) {
-        ending = "치료 성공";
-    } else if (player.rescuePoint >= 3 && player.date > 10) {
-        ending = "구조 성공";
-    } else if (player.date > 15) {
-        ending = "생존 성공";
-    } else {
-        return false;
-    }
-    showEnding(player,ending);
-    return true;
+function pickShuffleCard(cards) {
+  const index = Math.floor(Math.random() * cards.length);
+  const card = cards.splice(index, 1)[0];
+  if (card < 4) {
+    // 생존자 시체
+    return status.DEADBODY;
+  }
+  if (card < 8) {
+    // 부상당한 군인
+    return status.SOLDIER;
+  }
+  if (card < 11) {
+    // 임시수술
+    return status.OPERATION;
+  }
+  if (card < 14) {
+    // 군용 차량 행렬
+    return status.CAR;
+  }
+  if (card < 17) {
+    // 오염된 웅덩이
+    return status.POOL;
+  }
+  if (card < 20) {
+    // 구조 트럭
+    return status.RESCUETRUCK;
+  }
+  return 0;
 }
 
-export { shuffleCard,pickShuffleCard ,whatIsCardContent, checkEnding}
+export { pickShuffleCard, createCardPack, loadCardContent };
--- src/viewmodel/index.js
+++ src/viewmodel/index.js
@@ -1,124 +1,130 @@
-import { showTwoChoice } from "../view/index.js";
-import { continueGame } from "../view/index.js";
-import { showStates } from "../view/index.js";
-import { showEnding } from "../view/index.js";
-import { restartScreen } from "../view/index.js";
-import { shuffleCard } from "../model/index.js";
-import { pickShuffleCard } from "../model/index.js";
-import { whatIsCardContent } from "../model/index.js";
+import {
+  showTwoChoice,
+  continueGame,
+  showStates,
+  showEnding,
+  restartScreen,
+  hideLoading,
+  showLoading,
+} from '../view/index.js';
+
+import {
+  createCardPack,
+  pickShuffleCard,
+  loadCardContent,
+} from '../model/index.js';
+
 let player = {
+  date: 1,
+  hp: 100,
+  food: 3,
+  infection: 10,
+  heal: 0,
+  rescuePoint: 0,
+};
+let cards = [];
+let cardNum;
+
+function updateLogs(result) {
+  document.querySelector('#log').innerHTML += `<p>${result}</p>`;
+  return 0;
+}
+function caculateAfterDay(newState) {
+  newState.date += 1;
+  if (newState.food <= 0) {
+    newState.hp -= 10;
+    newState.food = 0;
+    updateLogs('식량이 없어 체력이 감소합니다.');
+  } else {
+    newState.food -= 1;
+  }
+  newState.infection += 3;
+  return newState;
+}
+
+function checkStates(newState) {
+  if (newState.food < 0) {
+    newState.food = 0;
+  }
+  if (newState.hp < 0) {
+    newState.hp = 0;
+  }
+  if (newState.infection < 0) {
+    newState.infection = 0;
+  }
+  return newState;
+}
+function checkEnding() {
+  let ending;
+  if (player.hp === 0) {
+    ending = '사망';
+  } else if (player.infection >= 100) {
+    ending = '좀비화';
+  } else if (player.heal >= 5) {
+    ending = '치료 성공';
+  } else if (player.rescuePoint >= 3 && player.date > 10) {
+    ending = '구조 성공';
+  } else if (player.date > 15) {
+    ending = '생존 성공';
+  } else {
+    return false;
+  }
+  showEnding(player, ending);
+  return true;
+}
+function calculateEffect(effect) {
+  let newState = { ...player };
+  for (let key in effect) {
+    newState[key] += effect[key];
+  }
+  return newState;
+}
+
+function updateStates(choice) {
+  hideLoading();
+  const card = loadCardContent(cardNum);
+  const effect = card[choice];
+  let newState = calculateEffect(effect);
+  updateLogs(card[`choice${choice}`]);
+  for (let key in effect) {
+    updateLogs(`${key}: ${effect[key]}`);
+  }
+  newState = checkStates(newState);
+  newState = caculateAfterDay(newState);
+  player = newState;
+  showStates(player);
+  continueGame();
+  checkEnding();
+}
+function restartGame() {
+  cards = createCardPack();
+  restartScreen(cards);
+  player = {
     date: 1,
     hp: 100,
     food: 3,
     infection: 10,
     heal: 0,
-    rescuePoint: 0
+    rescuePoint: 0,
+  };
+  showStates(player);
 }
-let cards = []
-let cardNum;
+function drawCard() {
+  cardNum = pickShuffleCard(cards);
 
-function drawCard() {   
-    cardNum = pickShuffleCard(cards);
-
-    let cardContent = whatIsCardContent(cardNum);
-    showTwoChoice(cardContent,cards);
-    if (!cards.length) {
-        cards = shuffleCard();
-    }
+  const cardContent = loadCardContent(cardNum);
+  showTwoChoice(cardContent, cards);
+  if (!cards.length) {
+    cards = createCardPack();
+  }
 }
 function clickChoiceBtn(choice) {
-    document.querySelector('#btn-choice-a').disabled = true;
-    document.querySelector('#btn-choice-b').disabled = true;
-    document.querySelector('#loading').classList.remove("hidden");
-    setTimeout(function () {
-        updateStates(choice);
-    },2000)
-
+  showLoading();
+  setTimeout(function () {
+    updateStates(choice);
+  }, 2000);
 }
-function updateStates(choice) {
-    document.querySelector('#loading').classList.add("hidden");
-    document.querySelector('#btn-choice-a').disabled = false;
-    document.querySelector('#btn-choice-b').disabled = false;
-    const card = whatIsCardContent(cardNum);
-    const effect = card[choice];
-    for (let key in effect) {
-        player[key] += effect[key];
-    }
-    updateLogs(card[`choice${choice}`]);
-    for(let key in effect){
-        updateLogs(`${key}: ${effect[key]}`);
-    }
-    checkStates();
-    // if(checkEnding()){return;} 리턴을 안하면 가끔 테스트 통과가 됩니다..
-    checkEnding();
-    afterDay();
-    showStates(player);
-    continueGame();
-    //if(checkEnding()){return;}
-    checkEnding();
-}
-function updateLogs(result) {
-    document.querySelector("#log").innerHTML+=`<p>${result}</p>`;
-    return 0;
-}
-function afterDay() {
-    player.date++;
-    if (player.food <= 0) {
-        player.hp -= 10;
-        player.food = 0;
-        updateLogs("식량이 없어 체력이 감소합니다.");
-    } else {
-        player.food -= 1;
-    }
-    player.infection += 3;
-}
-
-function checkStates() {
-    if (player.food < 0) {
-        player.food = 0;
-    }
-    if (player.hp < 0) {
-        player.hp = 0;
-    }
-    if (player.infection < 0) {
-        player.infection = 0;
-    }
-
-}
-function checkEnding() {
-    let ending;
-    if (player.hp === 0) {
-        ending = "사망";
-    } else if (player.infection >= 100) {
-        ending = "좀비화";
-    } else if (player.heal >= 5) {
-        ending = "치료 성공";
-    } else if (player.rescuePoint >= 3 && player.date > 10) {
-        ending = "구조 성공";
-    } else if (player.date > 15) {
-        ending = "생존 성공";
-    } else {
-        return false;
-    }
-    showEnding(player,ending);
-    return true;
-}
-
-function restartGame() {
-    cards = shuffleCard();
-    restartScreen(cards)
-    player = {
-        date: 1,
-        hp: 100,
-        food: 3,
-        infection: 10,
-        heal: 0,
-        rescuePoint: 0
-    }
-    showStates(player);
-}
-
-cards = shuffleCard();
+cards = createCardPack();
 
 const drawBtn = document.querySelector('#btn-draw');
 drawBtn.onclick = drawCard;
@@ -128,8 +134,7 @@ const choiceBtnB = document.querySelector('#btn-choice-b');
 const giveUpBtn = document.querySelector('#btn-giveup');
 const restartBtn = document.querySelector('#btn-restart');
 
-choiceBtnA.onclick = () => clickChoiceBtn("A");
-choiceBtnB.onclick = () => clickChoiceBtn("B");
-giveUpBtn.onclick = () => showEnding(player,"포기");
+choiceBtnA.onclick = () => clickChoiceBtn('A');
+choiceBtnB.onclick = () => clickChoiceBtn('B');
+giveUpBtn.onclick = () => showEnding(player, '포기');
 restartBtn.onclick = restartGame;
-
